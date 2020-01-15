01405fb278b3a354d6233a21b2bfd7c4cad4e05e
prudnikovv <prudnikovv@ukr.net>
2020-01-15 10:15:41 +0000

    Improve 55-3

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,5 @@
 class TagsController < ApplicationController
   def show
-    @questions = Tag.find(params[:id]).questions.order(created_at: :desc)
+    @questions = Tag.find_by(name: params[:id]).questions.order(created_at: :desc)
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,21 +1,26 @@
 class Question < ApplicationRecord
+  REGULAR_TAG = /#[[:word:]]+/
   belongs_to :user
   has_many :question_tags, dependent: :destroy
   has_many :tags, through: :question_tags, dependent: :destroy
 
   validates :text, presence: true, length: { maximum: 255 }
 
-  after_create_commit :save_tags
+  after_save_commit :save_tags
   after_destroy_commit :destroy_tags
 
   def text_show
     text.delete("#")
   end
 
+  def answer_show
+    answer.delete("#")
+  end
+
   private
 
   def find_all_tags
-    "#{text} #{answer}".scan(/#[а-яА-ЯёЁa-zA-Z0-9]+/)
+    "#{text} #{answer}".scan(REGULAR_TAG)
   end
 
   def save_tags
