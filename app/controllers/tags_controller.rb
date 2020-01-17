a3dc8cb4458a27ca8c377471158de1592b5fd95e
prudnikovv <prudnikovv@ukr.net>
2020-01-17 11:06:18 +0000

    improve 404

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,9 +1,5 @@
 class TagsController < ApplicationController
   def show
-    if Tag.friendly.exists?(params[:id])
-      @questions = Tag.friendly.find(params[:id]).questions.order(created_at: :desc)
-    else
-      render file: 'public/404', status: :not_found, :formats => [:html]
-    end
+    @questions = Tag.friendly.find(params[:id]).questions.order(created_at: :desc)
   end
 end
